ab31f81a5837edeb53d70b08acf667fc87900c99
Mateus Villain <mateusvillain@gmail.com>
2026-02-14 15:26:30 -0300

    chore(analytics): remove unused GA events

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -13,35 +13,12 @@ document.querySelectorAll('[data-link]').forEach(link => {
   });
 });
 
-// Tabs
-document.querySelectorAll('[role="tab"]').forEach(tab => {
-  tab.addEventListener('click', () => {
-    const label = tab.dataset.tab;
-
-    // Dispara o evento do GA4
-    sendEvent('tools_tab_click', {
-      tab_name: label
-    });
-  });
-});
-
 // Social links
 document.querySelectorAll('[data-social]').forEach(link => {
   link.addEventListener('click', () => {
     const name = link.dataset.social;
 
     sendEvent('social_link_click', {
-      platform: name
-    });
-  });
-});
-
-// Let's UI
-document.querySelectorAll('[data-lui]').forEach(link => {
-  link.addEventListener('click', () => {
-    const name = link.dataset.lui;
-
-    sendEvent('lui_link_click', {
       platform: name
     });
   });
